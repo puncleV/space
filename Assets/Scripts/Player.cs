6d7af29e2e118512d4de2c684a88bfb57c49f5a4
punkkk <punkrus18@gmail.com>
2019-06-25 18:59:52 +0300

    new movement limitation for a palyer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
-    [SerializeField] float maxX;
-    [SerializeField] float minX;
-    [SerializeField] float maxY;
-    [SerializeField] float minY;
+     float maxX;
+    float minX;
+    float maxY;
+    float minY;
     // Start is called before the first frame update
     void Start()
     {
-        
+        setUpWorldBoundaries();
     }
 
     // Update is called once per frame
     void Update()
     {
         move();
+    }
+
+    private void setUpWorldBoundaries ()
+    {
+        Camera gameCamera = Camera.main;
+
+        maxX = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
+        minX = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
+
+        maxY = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
+        minY = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
     }
 
     private void move()
