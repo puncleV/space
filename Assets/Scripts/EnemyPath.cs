1a88328db2a9e86ac2a22eef77148ff8bc0da473
punkkk <punkrus18@gmail.com>
2019-06-26 20:58:01 +0300

    add enemy pathing

---
+++ Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] List<GameObject> waypoints;
+    [SerializeField] float speed = 1f;
+
+    [SerializeField]  int targetWaypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[targetWaypointIndex].transform.position; 
+    }
+
+    private void Update()
+    {
+        Vector2 targetPosition = waypoints[targetWaypointIndex].transform.position;
+
+        if (Vector2.Distance(transform.position, targetPosition) < 0.05)
+        {
+            if (targetWaypointIndex == (waypoints.Count - 1))
+            {
+                Destroy(gameObject);
+            } else
+            {
+                targetWaypointIndex++;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+}
