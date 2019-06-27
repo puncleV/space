59ecd705e4d74a7a8794ffb3e49d94955223b3c9
punkkk <punkrus18@gmail.com>
2019-06-27 20:06:20 +0300

    add extra waves, better handling multiple waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,39 +6,32 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfig> waveConfigs;
     [SerializeField] int startWave = 0;
-    int currentWaveIndex;
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentWaveIndex = startWave;
-        StartCoroutine(startWaves());
-    }
+    [SerializeField] bool loopedWaves = true;
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Start()
     {
+        do
+        {
+            yield return StartCoroutine(startWaves());
+        } while (loopedWaves);
     }
 
     private IEnumerator startWaves ()
     {
-        while(true)
+        for(var i = 0; i < waveConfigs.Count; i++)
         {
-            var currentWave = waveConfigs[currentWaveIndex];
+            var currentWave = waveConfigs[i];
 
             yield return StartCoroutine(spawnEnemies(currentWave));
-
-            currentWaveIndex = (currentWaveIndex == waveConfigs.Count - 1) ? 0 : currentWaveIndex + 1;
         }
     }
 
     private IEnumerator spawnEnemies (WaveConfig wave)
     {
-        var currentWave = waveConfigs[currentWaveIndex];
-
         for (int i = 0; i < wave.EnemiesCount; i++)
         { 
             var newEnemy = Instantiate(wave.EnemyPrefab, wave.getWaypoints()[0].position, Quaternion.identity);
-            newEnemy.GetComponent<EnemyPath>().setWaveConfig(currentWave);
+            newEnemy.GetComponent<EnemyPath>().setWaveConfig(wave);
 
             yield return new WaitForSeconds(wave.TimeBetweenSpawns);
         }
