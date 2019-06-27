3051fa37fb1eaa3376610388b4c0f9b8b5b6ea4a
punkkk <punkrus18@gmail.com>
2019-06-27 19:45:43 +0300

    spawn enemies for multiple waves

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class EnemyPath : MonoBehaviour
 {
-    [SerializeField] WaveConfig waveConfig;
     [SerializeField] float speed = 1f;
     [SerializeField] int targetWaypointIndex = 0;
 
+    WaveConfig waveConfig;
     List<Transform> waypoints;
+
+    public void setWaveConfig (WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
     private void Start()
     {
         waypoints = waveConfig.getWaypoints();
@@ -36,6 +42,6 @@ public class EnemyPath : MonoBehaviour
             }
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, waveConfig.EnemySpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startWave = 0;
+    int currentWaveIndex;
+    Coroutine spawnCoroutine = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWaveIndex = startWave;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnCoroutine == null)
+        {
+            spawnCoroutine = StartCoroutine(spawnEnemies());
+        }
+    }
+
+    private IEnumerator spawnEnemies ()
+    {
+        var currentWave = waveConfigs[currentWaveIndex];
+
+        for (int i = 0; i < currentWave.EnemiesCount; i++)
+        { 
+            var newEnemy = Instantiate(currentWave.EnemyPrefab, currentWave.getWaypoints()[0].position, Quaternion.identity);
+            newEnemy.GetComponent<EnemyPath>().setWaveConfig(currentWave);
+
+            yield return new WaitForSeconds(currentWave.TimeBetweenSpawns);
+        }
+
+        currentWaveIndex = (currentWaveIndex == waveConfigs.Count - 1) ? 0 : currentWaveIndex + 1;
+
+        StopCoroutine(spawnCoroutine);
+        spawnCoroutine = null;
+    }
+}
