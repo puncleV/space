cd5acf259e4398898d462f6785ac45ca6aa06c1a
punkkk <punkrus18@gmail.com>
2019-07-12 07:55:38 +0300

    add enemies lazers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 100;
+    [SerializeField] float minTimeBetweenShots = 0.1f;
+    [SerializeField] float maxTimeBetweenShots = 1f;
+    [SerializeField] float baseLazerSpeed = 10f;
+    [SerializeField] GameObject lazerPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(autoFire());
     }
 
     // Update is called once per frame
@@ -17,19 +22,35 @@ public class Enemy : MonoBehaviour
         
     }
 
+    private IEnumerator autoFire()
+    {
+        while (true)
+        {
+            var shotPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            var shot = Instantiate(lazerPrefab, shotPosition, Quaternion.identity);
+            shot.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -baseLazerSpeed);
+
+            yield return new WaitForSeconds(Random.Range(minTimeBetweenShots, maxTimeBetweenShots));
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collider)
     {
         DamageDealer damageDealer = collider.gameObject.GetComponent<DamageDealer>();
-        if (damageDealer)
+        Debug.Log(damageDealer.tag);
+        if (damageDealer.tag != "enemy_lazer")
         {
-            Debug.Log(damageDealer.Damage);
-
-            this.health -= damageDealer.Damage;
-            damageDealer.hit();
+            handleHit(damageDealer);
         }
+    }
+
+    private void handleHit(DamageDealer damageDealer)
+    {
+        this.health -= damageDealer.Damage;
 
         if (this.health <= 0)
         {
+            StopAllCoroutines();
             Destroy(gameObject);
         }
     }
